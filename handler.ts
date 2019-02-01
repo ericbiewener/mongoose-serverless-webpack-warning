1e36c1774af09d563c5ca74c5fe831c46f52fa12
Eric Biewener <ebiewener@gmail.com>
2019-02-01 07:49:04 -0800

    recreating warning

---
+++ handler.ts
@@ -0,0 +1,14 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import mongoose from 'mongoose'
+
+console.log(mongoose)
+
+export const hello: APIGatewayProxyHandler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+}
